8ad59c843839c8b5d4d5276930ab24a6619b259a
Gauri Ragade <gauriragade98@gmail.com>
2023-01-06 22:01:26 +0530

    first commit,home project

---
+++ home/src/com/gauri/home/Home.java
@@ -0,0 +1,17 @@
+package com.gauri.home;
+
+public class Home {
+	int hollsq;
+	int bedroomsq;
+	int kichense;
+	static int colour;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Home houseNo1=new Home();
+		Home.colour = 22;
+		double Total_sq;
+		
+	}
+
+}
